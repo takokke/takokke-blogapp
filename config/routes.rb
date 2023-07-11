3c8dc37d1dd16d844351e1b8cbb8422ca511321b
takokke <takumicr1526@gmail.com>
2023-07-11 15:27:04 +0900

    UIを作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
-  # get '/' => 'home#index'      #home_controllerのindexメソッドを呼び出す
-  get '/about' => 'home#about' #home_controllerのaboutメソッドを呼び出す
+  root to: 'articles#index'    # get '/' => 'articles#index'　と一緒 
+                               #articles.rbのindexメソッドを呼ぶ'
+
 end
