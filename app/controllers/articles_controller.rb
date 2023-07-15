7795b3db894c25f8f73b3637b8886011b70861b8
takokke <takumicr1526@gmail.com>
2023-07-15 22:52:57 +0900

    new & create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,33 @@
 class ArticlesController < ApplicationController
     def index
         # render 'articles/index'
-        @articles = Article.all #一覧を表示するときはindexを使う   
+        @articles = Article.all #一覧を表示するときはindexを使う
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new()
+    end
+
+    def create
+        @article = Article.new(article_params)
+        #saveはDBにインスタンスを保存するメソッド
+        if @article.save 
+            #保存した記事の詳細ページを映す
+            redirect_to article_path(@article), notice: '保存しました'
+        else
+            flash.now[:error] = '保存できませんでした'
+            render :new
+        end
+    end
+
+    private
+    # ブラウザからサーバへ送られるデータ(params)のうち、titleとcontentのみサーバは保存する
+    # Strong Parameterを返却するメソッド
+    def article_params
+        params.require(:article).permit(:title, :content)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    #バリデーションチェック
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :title, length: {maximum: 50 }
 end
