c9d9997e8e820471b0e9c4c0dc3d8f4462e542ad
takokke <takumicr1526@gmail.com>
2023-08-03 18:20:07 +0900

    add custom validation

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+//jQuery導入時の変更
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+//ここまで
 module.exports = environment
